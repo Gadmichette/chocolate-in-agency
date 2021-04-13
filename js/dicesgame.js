5097b0c8f8dcd5fc1e2a060350825d2062b57a62
Gadmichette <44443362+Gadmichette@users.noreply.github.com>
2021-04-13 18:28:52 +0100

    js dice game done

--- js/dicesgame.js
+++ js/dicesgame.js
@@ -1,3 +1,87 @@
-let dice = Math.floor(Math.random()*10);
 
-console.log(dice);
+
+let playersResult = [];
+
+class Player{
+    constructor(name){
+        this._name = name;
+        this._gameWon= 0;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    get gameWon(){
+        return this._gameWon;
+    }
+
+    set gameWon(win){
+        if(win){
+            this._gameWon ++;
+        }
+    }
+
+
+    diceResult(){
+        let dice = Math.floor(Math.random()*10 + 1); 
+        return dice;
+    }
+
+    playerResult(){
+        const result = {
+            name: this._name,
+            diceresult: this.diceResult()
+        }
+
+        playersResult.push(result);
+        return result
+    }
+
+}
+
+const player1 = new Player('David');
+const player2 = new Player('Gaetan');
+
+let play= () =>{
+    playersResult = [];
+    player1.playerResult();
+    player2.playerResult();
+    playersResult.sort((a, b) =>  a.diceresult - b.diceresult);
+
+    if( playersResult[0].diceresult === playersResult[1].diceresult){
+        return console.log('Match nul');
+    }else if(playersResult[playersResult.length-1].name === player1.name){
+        player2.gameWon = false;
+        player1.gameWon = true;
+        return console.log('Player 1 win');
+    }else if(playersResult[playersResult.length-1].name === player2.name){
+        player1.gameWon = false;
+        player2.gameWon = true;
+        return console.log('Player 2 win');
+    }
+    
+}
+
+play();
+console.log(playersResult);
+console.log(player1.gameWon);
+console.log(player2.gameWon);
+play();
+console.log(playersResult);
+console.log(player1.gameWon);
+console.log(player2.gameWon);
+play();
+console.log(playersResult);
+console.log(player1.gameWon);
+console.log(player2.gameWon);
+play();
+console.log(playersResult);
+console.log(player1.gameWon);
+console.log(player2.gameWon);
+
+play();
+console.log(playersResult);
+console.log(player1.gameWon);
+console.log(player2.gameWon);
+
